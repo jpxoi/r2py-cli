cf5e511f567d2f2069bc561a39bdab05706c86bc
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-23 08:26:50 +0100

    Refactor logger implementation to use singleton pattern and improve log file management

--- logger.py
+++ logger.py
@@ -1,7 +1,30 @@
 import logging
+import os
+from datetime import datetime
 
-def setup_logging():
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(message)s"
-    )
+class Logger:
+    _instance = None
+
+    def __new__(cls, action='action', log_dir='logs'):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._setup(action, log_dir)
+        return cls._instance
+
+    def _setup(self, action, log_dir):
+        os.makedirs(log_dir, exist_ok=True)
+        timestamp = datetime.now().strftime('%Y%m%d-%H%M%S')
+        log_filename = f'{action}-{timestamp}.log'
+        log_path = os.path.join(log_dir, log_filename)
+        self.logger = logging.getLogger(action)
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+        file_handler = logging.FileHandler(log_path, mode='w')
+        file_handler.setFormatter(formatter)
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(stream_handler)
+
+    def get_logger(self):
+        return self.logger
