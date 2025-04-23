6ae765bda247d91c50aeddae24246b5627118de8
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-23 09:03:37 +0100

    Refactor logger import in downloader to use the correct utils.logger module path

--- downloader.py
+++ downloader.py
@@ -5,7 +5,7 @@ from typing import Optional
 from dotenv import load_dotenv
 import boto3
 from tqdm import tqdm
-from lib.logger import Logger
+from utils.logger import Logger
 
 logger = Logger('download').get_logger()
 
