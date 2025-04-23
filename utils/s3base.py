2a74744aeb9e47dac293928a362092f200aa622e
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-23 09:47:21 +0100

    Refactor S3Base class initialization and logger setup for improved clarity and functionality

--- utils/s3base.py
+++ utils/s3base.py
@@ -1,10 +1,27 @@
 import os
 import sys
+import boto3
 from utils.logger import Logger
 
-logger = Logger('s3base').get_logger()
+logger = Logger('s3Client').get_logger()
 
 class S3Base:
+    def __init__(self, endpoint_url, access_key, secret_key, region):
+        self.logger = logger
+        if region == 'auto':
+            self.logger.warning("Region set to 'auto'. Routing requests automatically.")
+            region = None
+        else:
+            self.logger.info(f"Using region: {region}")
+        self.logger.info("Creating S3 client...")
+        self.s3 = boto3.client(
+            service_name='s3',
+            endpoint_url=endpoint_url,
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            region_name=region,
+        )
+
     @staticmethod
     def get_env_var(name: str, default=None, required=False):
         value = os.getenv(name, default)
@@ -13,3 +30,7 @@ class S3Base:
             sys.exit(1)
         logger.debug(f"Environment variable '{name}' loaded successfully.")
         return value
+
+    @staticmethod
+    def get_logger():
+        return logger
